e32f8cdccfece8179647dfb9d8082abc92e4448e
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-23 10:35:34 +0200

    Creation of Character and Location models.

---
+++ src/main/java/com/wildcodeschool/APIchallenge/controller/CharacterController.java
@@ -0,0 +1,22 @@
+package com.wildcodeschool.APIchallenge.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@RestController
+public class CharacterController {
+
+
+    private static final String RICKANDMORTY_URL = "https://rickandmortyapi.com/api/";
+/*    WebClient webClient = WebClient.create(RICKANDMORTY_URL);*/
+
+
+
+
+/*    @GetMapping("/") {
+        public static final s
+
+        return "/";
+    }*/
+}
---
+++ src/main/java/com/wildcodeschool/APIchallenge/model/Character.java
@@ -0,0 +1,115 @@
+package com.wildcodeschool.APIchallenge.model;
+
+public class Character {
+
+    private Integer id;
+    private String name;
+    private String status;
+    private String species;
+    private String type;
+    private String gender;
+    private Location origin;
+    private Location location;
+    private String image;
+    private String[] episodes;
+    private String url;
+    private String created;
+
+    public Character() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Location getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(Location origin) {
+        this.origin = origin;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String[] getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(String[] episodes) {
+        this.episodes = episodes;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
