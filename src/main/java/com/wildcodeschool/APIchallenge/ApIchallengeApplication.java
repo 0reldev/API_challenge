e25d71c311e1bcc2f54f8dadf91a5cc7b11302c2
Aurélien Lonni <aurelien.lonni@gmail.com>
2020-06-23 10:03:52 +0200

    Project initiliazation

---
+++ src/main/java/com/wildcodeschool/APIchallenge/ApIchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.APIchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeschool/APIchallenge/ApIchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.APIchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
